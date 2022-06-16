5720e7ea8de031888bbb6223ce91abda5b1009c4
Alison Zerbe <alisonkzerbe@gmail.com>
2022-06-15 23:31:14 -0700

    adds changes to BST

--- assignment5/src/BST.java
+++ assignment5/src/BST.java
@@ -1,40 +1,46 @@
-
+package com.company;
 
 public class BST {
-	
+
 	private TreeNode root; //overall root
-	
+	private int size;
+
 	public BST() {
 		this.root = null;
 	}
-	
-	public void insert(int key, String value) {
+
+	public void insert(String key, String value) {
 		root = insert(root, key, value);
+		size+=1;
 	}
-	
-	private TreeNode insert(TreeNode root, int key, String value) {
+
+	private TreeNode insert(TreeNode root, String key, String value) {
 		if(root == null)
 			root = new TreeNode(key, value);
-		else if(key < root.key)
+
+		else if(root.key.compareTo(key) > 0)
 			root.left = insert(root.left, key, value);
-		else if(key > root.key)
+		else if(root.key.compareTo(key) < 0)
 			root.right = insert(root.right, key, value);
-		else //if key == root.key, update operation
-			root.value = value;
-			
+		else if(root.key.compareTo(key) == 0)
+			root.key = key;
+
 		return root;
 	}
-	
-	public void remove (int key) {
+
+	public void remove (String key) {
 		root = remove(root, key);
 	}
-	
-	private TreeNode remove(TreeNode root, int key) {
+
+	private TreeNode remove(TreeNode root, String key) {
 		if(root == null)
 			return null;
-		else if (key < root.key)
+//		else if (key < root.key)
+			//TODO: check that this the correct ordering of compareTo()
+		else if (root.key.compareTo(key) < 0)
 			root.left =remove (root.left, key);
-		else if (key > root.key)
+//		else if (key > root.key)
+		else if (root.key.compareTo(key) > 0)
 			root.right =remove(root.right, key);
 		else { //key == root.key, we remove key-value pair
 			if(root.right == null)
@@ -42,67 +48,67 @@ public class BST {
 			else if (root.left == null)
 				return root.right;
 			else { //a parent with two children, assume that we find the min of right subtree
+				//TODO: check that this string value of is working as expected
 				root.key = getMin(root.right);
 				root.value = search(root.right, root.key);
 				root.right = remove(root.right, root.key);
-				
 			}
 		}
 		return root;
 	}
-	
-	
-	public int getMin() {
+
+
+	public String getMin() {
 		return getMin(root);
 	}
-	
-	private int getMin(TreeNode root) {
+
+	private String getMin(TreeNode root) {
 		if(root != null) {
 			if(root.left == null)
 				return root.key;
 			else
 				return getMin(root.left);
 		}
-		
-		return -1;
+
+		return "NOT FOUND";
 	}
-	
-	
-	public int getMax() {
+
+
+	public String getMax() {
 		return getMax(root);
 	}
-	
-	private int getMax(TreeNode root) {
+
+	private String getMax(TreeNode root) {
 		if(root != null) {
 			if(root.right == null)
 				return root.key;
 			else
 				return getMax(root.right);
 		}
-		
-		return -1;
+
+		return "NOT FOUND";
 	}
 
-	
-	public String search(int key) {
+
+	public String search(String key) {
 		return search(root, key); //root => overall root
 	}
-	
-	private String search(TreeNode root, int key) { //root-> root of the subtree
+
+	private String search(TreeNode root, String key) {
 		if(root == null)  //key is not found
-			return null;
-		else if (key < root.key)  //key is on the left branch
+			return "NOT FOUND";
+		else if (root.key.compareTo(key) < 0)
 			return search(root.left, key);
-		else if (key > root.key)		//key is on the right branch
+		else if (root.key.compareTo(key) > 0)
 			return search(root.right, key);
-		
+
 		return root.value; 	//key == root.key, key is found
 	}
-	
+
 	public void printInOrder() {
 		printInOrder(root);
 	}
-	
+
 	//recursive print in order function
 	private void printInOrder(TreeNode root) {
 		if(root != null) {
@@ -111,11 +117,11 @@ public class BST {
 			printInOrder(root.right);
 		}
 	}
-	
+
 	public void printPreOrder() {
 		printPreOrder(root);
 	}
-	
+
 	//recursive print pre order function
 	private void printPreOrder(TreeNode root) {
 		if(root != null) {
@@ -124,12 +130,11 @@ public class BST {
 			printPreOrder(root.right);
 		}
 	}
-	
-	
+
 	public void printPostOrder() {
 		printPostOrder(root);
 	}
-	
+
 	//recursive print post order function
 	private void printPostOrder(TreeNode root) {
 		if(root != null) {
@@ -138,11 +143,11 @@ public class BST {
 			System.out.println(root.key +":"+root.value);
 		}
 	}
-	
+
 	public int leafCount() {
 		return leafCount(root);
 	}
-	
+
 	private int leafCount(TreeNode root) {
 		if(root == null)
 			return 0;
@@ -151,11 +156,11 @@ public class BST {
 		else
 			return leafCount(root.left) + leafCount(root.right);
 	}
-	
+
 	public int countParents() {
 		return countParents(root);
 	}
-	
+
 	private int countParents(TreeNode root) {
 		if(root == null | (root.left == null && root.right == null))
 			return 0;
@@ -164,18 +169,18 @@ public class BST {
 		else {
 			return 1 + countParents(root.left) + countParents(root.right);
 		}
-			
+
 	}
-	
-	
+
+
 	//inner class for a tree node
 	private class TreeNode{
-		public int key;
+		public String key;
 		public String value;
 		public TreeNode left;
 		public TreeNode right;
-		
-		public TreeNode(int key, String value) {
+
+		public TreeNode(String key, String value) {
 			this.key = key;
 			this.value = value;
 			this.left = null;
@@ -183,4 +188,7 @@ public class BST {
 		}
 	}
 
+	public int getSize() {
+		return size;
+	}
 }
